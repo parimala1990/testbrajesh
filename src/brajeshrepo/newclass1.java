237dabc98861272b2e026c2b59c53b4c6f85ac17
INFOR\pbalaraju <parimala.balaraju@infor.com>
2019-01-16 14:25:00 +0530

    brajesh first repo

---
+++ src/brajeshrepo/newclass1.java
@@ -0,0 +1,12 @@
+package brajeshrepo;
+
+public class newclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test 1 repo");
+
+	}
+
+}
